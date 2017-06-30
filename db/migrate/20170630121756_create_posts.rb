56f0e1e4ee68da6f3f35db76594de0c99accf3ba
huranga <hugo.pato@hotmail.fr>
2017-06-30 14:45:02 +0200

    Creation model/controller User

--- db/migrate/20170630121756_create_posts.rb
+++ db/migrate/20170630121756_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :tilte
-      t.data :publishDate
+      t.date :publishDate
       t.text :hat
       t.text :content
       t.string :picture
