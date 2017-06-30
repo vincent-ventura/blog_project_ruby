4aeb00aa4c7b7cb8d36eac62c10b469788e78ae6
vventura <vincent.ventura1@gmail.com>
2017-06-30 16:13:33 +0200

    update posts

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
 
+gem 'carrierwave', '~> 1.0'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630131632) do
+ActiveRecord::Schema.define(version: 20170630141305) do
 
-  create_table "Posts", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.date     "publishDate"
     t.text     "hat"
     t.text     "content"
-    t.string   "picture"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "avatar"
   end
 
   create_table "users", force: :cascade do |t|
